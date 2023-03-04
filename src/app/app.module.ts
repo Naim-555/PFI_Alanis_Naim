cda1f221a0ef4a44fe2b86c97105629a540b57c1
naim <naimalanis555@gmail.com>
2023-03-04 20:35:39 -0300

    Solución de Problemas
    Solución de Problemas con angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { SocialComponent } from './components/social/social.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LogoAPComponent,
+    AcercaDeComponent,
+    BannerComponent,
+    ExperienciaComponent,
+    SocialComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
