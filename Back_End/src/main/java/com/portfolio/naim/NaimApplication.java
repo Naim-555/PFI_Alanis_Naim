c0beb6192f6357e6b654e5c71ceebd332037ce8b
naim <naimalanis555@gmail.com>
2023-03-10 22:25:16 -0300

    So
    que

---
+++ Back_End/src/main/java/com/portfolio/naim/NaimApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.naim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaimApplication.class, args);
+	}
+
+}
